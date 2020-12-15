cdce986559028dd2cf1e36f64e5d4f0bdb2b8233
PauLOk777 <trocjuk2000@gmail.com>
2020-12-15 16:43:12 +0200

    Fixed some bugs

--- src/main/java/com/paulok777/lab4/Planner.java
+++ src/main/java/com/paulok777/lab4/Planner.java
@@ -11,10 +11,15 @@ public class Planner {
     private int tasks;
     private int resources;
 
+    private List<Integer> optimizedTasks;
+    private List<Integer> optimizedResources;
+
     public Planner(int tasks, int resources) {
         this.tasks = tasks;
         this.resources = resources;
         matrix = new int[tasks][resources];
+        optimizedTasks = new ArrayList<>();
+        optimizedResources = new ArrayList<>();
     }
 
     public void printMatrix() {
@@ -51,96 +56,61 @@ public class Planner {
     }
 
     public void plan() {
-        List<Integer> optimizedPairs = new ArrayList<>();
-
-        searchAndOptimizeImportantNodes(0, optimizedPairs);
+        searchAndOptimizeImportantNodesByRow(optimizedTasks, optimizedResources);
+        searchAndOptimizeImportantNodesByColumn(optimizedTasks, optimizedResources);
 
         for (int i = 0; i < tasks; i++) {
-            if (!optimizedPairs.contains(i)) {
+            if (!optimizedTasks.contains(i)) {
                 for (int j = 0; j < resources; j++) {
-                    if (matrix[i][j] > 0) {
+                    if (!optimizedResources.contains(j) && matrix[i][j] > 0) {
                         reduceMatrix(i, j);
                         printMatrix();
-                        optimizedPairs.add(i);
-                        searchAndOptimizeImportantNodes(i + 1, optimizedPairs);
+                        optimizedTasks.add(i);
+                        optimizedResources.add(j);
+                        System.out.println("Optimized tasks list: " + optimizedTasks);
+                        System.out.println("Optimized resources list: " + optimizedResources);
+                        searchAndOptimizeImportantNodesByRow(optimizedTasks, optimizedResources);
+                        searchAndOptimizeImportantNodesByColumn(optimizedTasks, optimizedResources);
                     }
                 }
             }
         }
     }
 
-    private void searchAndOptimizeImportantNodes(int startTask, List<Integer> optimizedPairs) {
-        for (int i = startTask; i < tasks; i++) {
+    private void searchAndOptimizeImportantNodesByRow(List<Integer> optimizedTasks, List<Integer> optimizedResources) {
+        for (int i = 0; i < tasks; i++) {
             int resourceIndex = getResourceIndex(i);
+            if (optimizedResources.contains(resourceIndex)) break;
             if (resourceIndex != -1) {
+                System.out.println("RowSearch. TaskIndex: " + i + ". ResourceIndex: " + resourceIndex);
                 reduceMatrix(i, resourceIndex);
                 printMatrix();
-                optimizedPairs.add(i);
-                continue;
+                optimizedTasks.add(i);
+                optimizedResources.add(resourceIndex);
+                System.out.println("Optimized tasks list: " + optimizedTasks);
+                System.out.println("Optimized resources list: " + optimizedResources);
             }
+        }
+    }
 
+    private void searchAndOptimizeImportantNodesByColumn(List<Integer> optimizedTasks, List<Integer> optimizedResources) {
+        for (int i = 0; i < resources; i++) {
             int taskIndex = getTaskIndex(i);
+            if (optimizedTasks.contains(taskIndex)) break;
             if (taskIndex != -1) {
+                System.out.println("ColumnSearch. ResourceIndex: " + i + ". TaskIndex: " + taskIndex);
                 reduceMatrix(taskIndex, i);
                 printMatrix();
-                optimizedPairs.add(taskIndex);
+                optimizedResources.add(i);
+                optimizedTasks.add(taskIndex);
+                System.out.println("Optimized tasks list: " + optimizedTasks);
+                System.out.println("Optimized resources list: " + optimizedResources);
             }
-        }
-    }
-
-    private int getResourceIndex(int currentTask) {
-        int indexOfResource = -1;
-        for (int j = 0; j < resources; j++) {
-            if (matrix[currentTask][j] > 0 && indexOfResource == -1) {
-                indexOfResource = j;
-            }
-
-            if (matrix[currentTask][j] > 0) {
-                return -1;
-            }
-        }
-        return indexOfResource;
-    }
-
-    private int getTaskIndex(int currentResource) {
-        int indexOfTask = -1;
-        for (int j = 0; j < tasks; j++) {
-            if (matrix[j][currentResource] > 0 && indexOfTask == -1) {
-                indexOfTask = j;
-            }
-
-            if (matrix[j][currentResource] > 0) {
-                return -1;
-            }
-        }
-
-        return indexOfTask;
-    }
-
-    public Map<Integer, Integer> getRelationTaskResource() {
-        Map<Integer, Integer> relation = new HashMap<>();
-
-        for(int i = 0; i < tasks; i++) {
-            for (int j = 0; j < resources; j++) {
-                if (matrix[i][j] > 0) {
-                    relation.put(i, j);
-                    break;
-                }
-            }
-        }
-
-        return relation;
-    }
-
-    public void printRelationTaskResource(Map<Integer, Integer> relation) {
-        System.out.println("Task -> Resource");
-        for (Map.Entry<Integer, Integer> pair: relation.entrySet()) {
-            System.out.println(pair.getKey() + " -> " + pair.getValue());
         }
     }
 
     private void reduceMatrix(int row, int column) {
-        for (int i = 0; i < tasks; i++) {
+        for (int i = 0; i < resources; i++) {
             if (i != column) {
                 matrix[row][i] = 0;
             }
@@ -153,13 +123,45 @@ public class Planner {
         }
     }
 
+    private int getResourceIndex(int currentTask) {
+        int indexOfResource = -1;
+        for (int j = 0; j < resources; j++) {
+            if (matrix[currentTask][j] > 0 && indexOfResource == -1) {
+                indexOfResource = j;
+            } else if (matrix[currentTask][j] > 0) {
+                return -1;
+            }
+        }
+        return indexOfResource;
+    }
+
+    private int getTaskIndex(int currentResource) {
+        int indexOfTask = -1;
+        for (int j = 0; j < tasks; j++) {
+            if (matrix[j][currentResource] > 0 && indexOfTask == -1) {
+                indexOfTask = j;
+            } else if (matrix[j][currentResource] > 0) {
+                return -1;
+            }
+        }
+
+        return indexOfTask;
+    }
+
+    public void printRelationTaskResource() {
+        System.out.println("Task -> Resource");
+        for (int i = 0; i < optimizedResources.size(); i++) {
+            System.out.println(optimizedTasks.get(i) + " -> " + optimizedResources.get(i));
+        }
+    }
+
     public static void main(String[] args) {
-        Planner planner = new Planner(5, 5);
+        Planner planner = new Planner(8, 5);
         planner.printMatrix();
-//        planner.randomFill();
-        planner.fillByExample();
+        planner.randomFill();
+//        planner.fillByExample();
         planner.printMatrix();
         planner.plan();
-        planner.printRelationTaskResource(planner.getRelationTaskResource());
+        planner.printRelationTaskResource();
     }
 }
